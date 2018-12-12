2a3ff2c7b5c8fe0923d5480a647802a3bdf2695f
cutsi <cuturacutura.00@gmail.com>
2018-12-12 22:41:17 +0100

    Created project

---
+++ DomacaZadaca-03/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DomacaZadaca_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
