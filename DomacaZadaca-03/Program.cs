ee2aed2fcec92f01a6f05bf51e7cd92e0625350b
cutsi <cuturacutura.00@gmail.com>
2018-12-12 22:45:53 +0100

    changed program

--- DomacaZadaca-03/Program.cs
+++ DomacaZadaca-03/Program.cs
@@ -7,6 +7,7 @@ namespace DomacaZadaca_03
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("test");
         }
     }
 }
